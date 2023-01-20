45a906bccd6b84813aa92d893cdaf541c771c541
Sumaya hirsi <hirsisumaya@gmail.com>
2023-01-20 19:19:46 +0000

    *

---
+++ Practice.js
@@ -0,0 +1,58 @@
+console.log("hello welcome to the first lesson of javasrcipt")
+
+var name = "sumaya"
+
+var age = 21
+
+console.log(age)
+
+// String Concatenation
+
+console.log(name)
+console.log("hello " + name )
+console.log("Hi my " + name + " is sumaya and i am " + age + " years old.")
+
+// Number concatenation
+
+console.log(2+2/2)
+
+//More varialbles
+
+var today = "Saturday"
+// var is for what is constantly changing.
+let tomorrow = "Sunday"
+//Let and var are same,but let is usef for reassigning things.
+const myName = "Sumaya"
+// Constants will never chnage age,gender, eyecolor etc.
+tomorrow = "Tuesday"
+
+console.log(tomorrow)
+//This has reassigned "tomorrow"
+
+tomorrow = "Monday"
+console.log( typeof tomorrow)
+/*This will tell you which datatype it is.
+Data types:
+-String
+-Object
+-Number
+-Undefined
+-Null
+*/
+
+function obj(){
+    //
+}
+
+var arr = ["apple", "strawberry", "banana", "grapes" ,1]
+console.log(arr)
+console.log(typeof arr)
+
+console.log(arr[2])
+//Gives you the second position
+
+console.log("The number of items in this list is" + arr.length)
+console.log(arr[25])
+
+
+
---
+++ hw1.js
@@ -0,0 +1,42 @@
+//Q1
+
+var string1 = "Hello"
+var string2 = "World"
+
+console.log(string1 + " " + string2)
+//Q2
+
+var number = 1
+var string = "Everyone"
+
+//Q3
+//console.log(string1+ " " + string2+ " " +string )
+
+console.log(`This is a sentence which uses three strings; the strings are ${string1} and ${string2} and ${string} this is called interpolation.`)
+
+//Q4
+console.log(typeof number)
+console.log(typeof string)
+
+//Q5
+
+var string3 = ("apples" , "orange" ,"kiwi", "banana" ,"papaya", "pomegranate", "plum","grapes","pineapple","strawberry")
+
+//Q6
+
+var stringArrays =["apples" , "orange" ,"kiwi", "banana" ,"papaya", "pomegranate", "plum","grapes","pineapple","strawberry"]
+
+stringArrays.pop()
+
+console.log(stringArrays)
+
+//Q7
+
+stringArrays.push("cherries")
+console.log(stringArrays)
+
+//Q8
+console.log(stringArrays[0])
+console.log(stringArrays[stringArrays.length-1])
+console.log(stringArrays[5])
+
